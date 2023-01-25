5f1ae3436fbd18d35395364b2c8da93ba53f7c3a
Khaled <khaledbm12@gmail.com>
2023-01-25 23:49:45 +0100

    Finished Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+   let containter = "";
+   if(num < 0 ) {
+      return "ERROR"
+   }
+   else {
+      for (let i = 0; i < num; i++) {
+         containter += string;
+      }
+      return containter;
+   };
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
