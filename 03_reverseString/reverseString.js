a05569b8e784e152e84fc55f06cba7bfa5ac6f12
Khaled <khaledbm12@gmail.com>
2023-01-26 16:06:46 +0100

    Passed First test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
 const reverseString = function(string) {
     let reverse = "";
-    for (let i = string.length; i >= 0; i--) {
+    for (let i = string.length - 1; i >= 0; i--) {
         reverse += string[i]
     }
     return reverse;
