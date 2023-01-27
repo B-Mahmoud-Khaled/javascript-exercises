e0586fdf9f2dac53c411872b1c646d00b0e21eb9
Khaled <khaledbm12@gmail.com>
2023-01-27 11:14:14 +0100

    Passed First test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ele1, ele2) {
+    for (let i = 0; i < array.length; i++) {
+        if ((array[i] === ele1) || (array[i] === ele2)) {
+            array.splice(i, 1);
+        }        
+    }
+    return array;
 };
 
 // Do not edit below this line
