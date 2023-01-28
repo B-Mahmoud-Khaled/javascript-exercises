e3cc0f2df98304b302b007350e92a244ad3de1ef
Khaled <khaledbm12@gmail.com>
2023-01-28 23:57:42 +0100

    Finished Excersise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,22 @@
-const sumAll = function(firstNumber, lastNumber) {
+const sumAll = function (n1, n2) {
     let sum = 0;
-    for (let i = firstNumber; i <= lastNumber; i++) {
-        sum += i;
-        
-    }
-    return sum;
+    if ((typeof (n1) !== "number") || (typeof (n2) !== "number") || (n1 < 0) || (n2 < 0))  {
+        return "ERROR";
+    } 
+    if (n1 > n2) {
+        for (let i = n2; i <= n1; i++) {
+            sum += i;
+        }
+        return sum; 
+    } else {
+        for (let i = n1; i <= n2; i++) {
+            sum += i;
+        }
+        return sum; 
+    }        
 };
+
+sumAll(1, 4)
 
 // Do not edit below this line
 module.exports = sumAll;
