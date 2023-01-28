cc32fa6608c8384ca766f8e83ff3843494f8e13f
Khaled <khaledbm12@gmail.com>
2023-01-29 00:20:31 +0100

    Finished Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function (temp) {
+  return round(((temp - 32) * (5/9)), 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  return round((temp * (9 / 5) + 32), 1)
 };
 
 // Do not edit below this line
